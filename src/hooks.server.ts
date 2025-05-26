adb83807f3785a1e6355851ba123b1691ec967bf
seongto <imseongto@gmail.com>
2025-05-26 15:41:10 +0900

    header uuid 키 이름 수정

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -11,7 +11,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 
         // company uuid 를 통해 정보 가져와서 검증하고, 실패시 401 에러 반환하는 로직 필요.
         if (event.url.pathname !== '/api/company/new') {
-            const companyId = event.request.headers.get('x-company-id');
+            const companyId = event.request.headers.get('X-Company-ID');
         
             if (!companyId) {
                 return errorResponse('회사의 uuid 정보가 필요합니다.', 401);
